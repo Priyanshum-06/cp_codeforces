e3231def721d52b993e21fdd0289433cb5e4fe62
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-06-28 11:18:33 +0530

    first commit

---
+++ cf_800/unique_num.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	string str;
+	cin >> str;
+	unordered_set<char> s;
+    for (int i = 0; i < str.size(); i++) {
+        s.insert(str[i]);
+    } 
+    cout << s.size() << endl;
+}
