0984f9b2e9e020c2da3a346e4fe3d5fe885333f2
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-07-01 07:12:00 +0530

    Day 4 done!

---
+++ cf_800/square_year.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int sq = ceil(sqrt(n));
+    if (sq * sq == n) {
+        cout << 0 << ' ' << sq << "\n";
+    } else {
+        cout << "-1\n";
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
