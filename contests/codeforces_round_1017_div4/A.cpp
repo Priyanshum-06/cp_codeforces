7aa46c6a723f5c52d1699ecc9dac02de0b3e86ee
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-07-13 11:50:48 +0530

    contest problems analyis done!

---
+++ contests/codeforces_round_1017_div4/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        string A, B, C;
+        cin >> A >> B >> C;
+        cout << A.front() << B.front() << C.front() << '\n';
+    }
+    return 0;
+}
