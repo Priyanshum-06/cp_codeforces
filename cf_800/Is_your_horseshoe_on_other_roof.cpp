fc67ba061a781d3c933970504a3b016db6370332
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-07-23 16:48:24 +0530

    coding done!

---
+++ cf_800/Is_your_horseshoe_on_other_roof.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+    set<int> colors = {s1, s2, s3, s4};
+    cout << 4 - colors.size() << '\n';
+    return 0;
+}
---
+++ cf_800/Ultra_Fast_Mathematicians.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string n1, n2;
+    cin >> n1 >> n2;
+    string ans = "";
+    for (int i = 0; i < n1.size(); i++) {
+    	// xor of each digit and then appending it
+    	if (n1[i] == n2[i]) ans += '0';
+    	else ans += '1';
+	}
+	cout << ans << '\n'; 
+    return 0;
+}
