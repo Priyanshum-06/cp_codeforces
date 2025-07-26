40e65626a8ca8aa4337b7076f2f307c34354d16a
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-07-26 08:34:45 +0530

    coding done!

---
+++ cf_800/Black_Square.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a[4];
+    for (int i = 0; i < 4; i++) {
+        cin >> a[i];
+    }
+    string s;
+    cin >> s;
+    int total_calories = 0;
+    for (char c : s) {
+        int index = c - '1';  
+        total_calories += a[index];
+    }
+    cout << total_calories << endl;
+    return 0;
+}
