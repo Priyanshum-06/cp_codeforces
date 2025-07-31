e91b67db035c301385d79e2ee5a1ef62288d8c98
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-08-01 00:57:50 +0530

    coding done!

--- cf_800/Even_Odds.cpp
+++ cf_800/Even_Odds.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 int main() {
     int n, k;
-    cin >> n >> k;
-    // taking n nums --> 10
-    for (int i = 1; i <= n; i++) {
-        // taking odd num from 1 to 10    ---> 1, 3, 5, 7, 9
-        for (int i = 1; i <= n; i+=2) {
-            cout << i << ' ';
-        }
-        
-        // taking even nums from 1 to 10 ----> 2, 4, 6, 8, 10
-        
+    cin >> n >> k;   // --> 10, 3 ==> 5
+
+    vector<int> merged;      // creating a vector container for storing the values given by the for loop
+
+    // taking odd num from 1 to 10    ---> 1, 3, 5, 7, 9
+    for (int i = 1; i <= n; i+=2) {
+        merged.push_back(i);
     }
-    return 0;
+        
+    // taking even nums from 1 to 10 ----> 2, 4, 6, 8, 10
+    for (int i = 2; i <= n; i+=2) {
+        merged.push_back(i);
+    }
+
+//     By combining---> 1, 3, 5, 7, 9, 2, 4, 6, 8, 10
+//     for (int x : merged) {
+//        cout << x << " "; 
+//    }
+    
+    cout << merged[k-1];
+	
+	return 0;
 }
