d710e24aae5ea5df82d5fad999c4cf19ce046543
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-08-01 01:22:23 +0530

    coding done!

--- cf_800/Even_Odds.cpp
+++ cf_800/Even_Odds.cpp
@@ -25,3 +25,21 @@ int main() {
 	
 	return 0;
 }
+
+
+// 2nd method for larger inputs and for avoidig the TLE
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int n, k;
+    cin >> n >> k;
+    long long int num_odds = (n + 1) / 2;
+    if (k <= num_odds) {
+        // k-th element is from odd numbers
+        cout << 2 * k - 1 << endl;
+    } else {
+        // k-th element is from even numbers
+        cout << 2 * (k - num_odds) << endl;
+    }
+    return 0;
+}
