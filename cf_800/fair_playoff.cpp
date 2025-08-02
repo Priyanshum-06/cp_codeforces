9489da4bd99a2d995a029584d6d53937a8e438a2
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-08-03 00:39:52 +0530

    coding done!

--- cf_800/fair_playoff.cpp
+++ cf_800/fair_playoff.cpp
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
 int main() {
-    int t;
-    cin >> t;
+    int t; cin >> t;
     while (t--) {
-        int f_men = 0;
-        int s_men = 0;
-        int s[4];
-        int slargest = -1;
-        int largest = s[0];
-        for (int i = 0; i < 4; i++) {
-            cin >> s[i];
-        }
-        for (int i = 0; i < 4; i++) {
-            f_men = max(s[0], s[1]);
-            s_men = max(s[2], s[3]);
-            if (slargest < s[i] && s[i] != largest) {
-                slargest = s[i];                
-            }
-            if (f_men == slargest || s_men == slargest) {
-                cout << "YES" << '\n';
-            }
-            else {
-                cout << "NO" << '\n';
-            }
+        vector<int> s(4);
+        for (int i = 0; i < 4; i++) cin >> s[i];
+
+        int semi1 = max(s[0], s[1]);  // Winner of first match
+        int semi2 = max(s[2], s[3]);  // Winner of second match
+
+        // Find the two highest skill values
+        vector<int> sorted_s = s;
+        sort(sorted_s.begin(), sorted_s.end());
+
+        int top1 = sorted_s[3];
+        int top2 = sorted_s[2];
+
+        // If the two winners are the top two skilled players
+        if ((semi1 == top1 || semi1 == top2) && (semi2 == top1 || semi2 == top2)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
         }
     }
     return 0;
