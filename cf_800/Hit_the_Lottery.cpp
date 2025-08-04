19eb2b76d667adaba2eb502dd0df0fa03e58b34e
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-08-04 21:58:01 +0530

    coding done!

---
+++ cf_800/Hit_the_Lottery.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    int denominations[] = {100, 20, 10, 5, 1};
+    for (int i = 0; i < 5; ++i) {
+        count += n / denominations[i];  
+        n %= denominations[i];          
+    }
+    cout << count << endl;
+    return 0;
+}
