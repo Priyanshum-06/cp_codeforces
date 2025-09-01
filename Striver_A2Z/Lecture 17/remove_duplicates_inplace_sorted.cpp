0fa1ab180c7e8fd3e7a04e9982e59cc99a800a0d
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-09-01 21:10:45 +0530

    coding done!

--- Striver_A2Z/Lecture 17/remove_duplicates_inplace_sorted.cpp
+++ Striver_A2Z/Lecture 17/remove_duplicates_inplace_sorted.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 	// remove duplicates in-place from sorted array
+    // brute force approach
 	vector<int> arr = {1, 1, 2, 2, 2, 3, 3};
 	set<int> st;
 	for (int i = 0; i < arr.size(); i++) {
@@ -17,4 +18,14 @@ int main() {
 	for (int i = 0; i < index; i++) {
 		cout << arr[i] << " ";
 	}
+
+    // optimal approach
+    int i = 0;
+	for (int j = 1; j < arr.size(); j++) {
+		if (arr[i] != arr[j]) {
+			arr[i+1] = arr[j];
+			i++;
+		}
+	}
+	cout << i+1 << '\n';
 }
