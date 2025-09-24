743997f437bdc53d0f39bf199c0a1c1548027380
Priyanshu Mishra <priyanshu.23cse@bmu.edu.in>
2025-09-24 12:59:32 +0530

    done!

---
+++ cf/282A.cpp
@@ -0,0 +1,26 @@
+/** 
+  *     author: pmcoder1729
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), 
+    cin.tie(0);
+    long long int n;
+    cin >> n;
+    string s;
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        if (s[1] == '+') {
+            x++;
+        }
+        else {
+            x--;
+        }
+    }
+    cout << x << '\n';
+    return 0;
+}
